9e24cc48a16612c1d3031ce66e19b0e1e6b3cd0f
kyluke mcdougall <=>
2024-10-06 15:07:05 +0200

    update link

--- termai.rb
+++ termai.rb
@@ -1,8 +1,8 @@
 class Termai < Formula
   desc "Terminal-based AI assistant in Rust"
   homepage "https://github.com/kyco/termai"
-  url "https://github.com/kyco/termai/releases/download/v0.1.0/termai"
-  sha256 ""
+  url "https://github.com/kyco/termai/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "ef2b2f55d1644ad3784b394b4aa89b59a9eef58a5c8348ea55839ac750759302"
   version "0.1.0"
 
   def install
